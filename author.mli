4a9bc01c2f6c96c4d3101febf1e63ad1f5efecb7
Andrew Chang <yc723@cornell.edu>
2022-03-05 02:33:50 -0500

    project initialization

---
+++ author.mli
@@ -0,0 +1,8 @@
+(** CS 3110 Spring 2022 Final Project
+    @author Andrew Chang (yc723) 
+    @author Katherine Heatzig (klh268)
+    @author Cathryn Li (cl893) 
+    @author Jessica Wang (jjw255) *)
+
+ val hours_worked : int
+ 
