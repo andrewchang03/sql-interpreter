e541d5882efb0eaf30c8af8f016d44daf5dce4e1
Jessica <jjw255@cornell.edu>
2022-03-12 21:10:51 -0500

    Created table.mli and table.ml
    - wrote mli for table with create and update functions
    - added unimplemented functions to table.ml

---
+++ src/table.mli
@@ -0,0 +1,29 @@
+open Command
+
+(** Representation of static table data.
+
+    This module represents the data stored in csv files. It handles
+    loading of that data as well as querying the data. *)
+
+(**********************************************************************
+ * DO NOT CHANGE THIS FILE
+ * It is part of the interface the course staff will use to test your
+ * submission.
+ **********************************************************************)
+
+type t
+(** The abstract type of values representing tables. *)
+
+val create_table : t -> string
+(** [create_table t] is the name of the file that the table [t] is in. *)
+
+val drop_table : t -> unit
+(** [drop_table t] deletes the table [t]. *)
+
+val update : t -> string list -> string list -> ('a -> 'b -> bool) -> t
+(** [update t s v c] is the updated table with the columns [s] updated
+    to the values [v] where the condition [c] is true. *)
+
+val insert : t -> string list -> string list -> t
+(** [insert t c v] is the table [t] with a new row with values [v] in
+    the respective columns [c] inserted at the beginning of the table*)
