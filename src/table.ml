794259621f7433f0fe16afabe7a9f78c05fe12d2
Jessica <jjw255@cornell.edu>
2022-03-15 21:42:10 -0400

    Added hours worked

--- src/table.ml
+++ src/table.ml
@@ -3,7 +3,7 @@ open Csv
 type t = string list list
 
 let create_table fname =
-  Csv.save ("data" ^ Filename.dir_sep ^fname^".csv") []
+  Csv.save ("data" ^ Filename.dir_sep ^ fname ^ ".csv") []
 
 let drop_table t =
   raise (Stdlib.Failure "Unimplemented: Table.drop_table")
@@ -14,4 +14,8 @@ let update t col vals cond =
 let insert t col vals =
   raise (Stdlib.Failure "Unimplemented: Table.insert")
 
+let swap_rows t =
+  raise (Stdlib.Failure "Unimplemented: Table.drop_table")
 
+let swap_cols t =
+  raise (Stdlib.Failure "Unimplemented: Table.drop_table")
