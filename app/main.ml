750bd3c3fc3754c1399e5e752dea977ae016a1ee
cathli66 <cathrynli606@gmail.com>
2022-04-30 17:07:43 -0400

    Wrote left and right arrow keys

--- app/main.ml
+++ app/main.ml
@@ -1,24 +1,24 @@
 open Database
 open Interface
 
-let appController game keyToAction ~key ~x ~y =
+let appController state keyToAction ~key ~x ~y =
   match keyToAction ~key ~x ~y with
-  | Some action -> game := Interface.controller !game action
+  | Some action -> Interface.controller !state action
   | None -> ()
 
 let arrowKeys ~key ~x ~y =
   match key with
-  | Glut.KEY_LEFT -> Some (Interface.move LEFT)
-  | Glut.KEY_RIGHT -> Some (Interface.move RIGHT)
-  | Glut.KEY_UP -> Some (Interface.move DOWN)
-  | Glut.KEY_DOWN -> Some (Interface.move UP)
+  | Glut.KEY_LEFT -> Some (Interface.Move LEFT)
+  | Glut.KEY_RIGHT -> Some (Interface.Move RIGHT)
+  | Glut.KEY_UP -> Some (Interface.Move DOWN)
+  | Glut.KEY_DOWN -> Some (Interface.Move UP)
   | _ -> None
 
 (* Declare rendering function, buffering mode, and create window *)
 let initDisplay ~w ~h ~title =
   Glut.initDisplayMode ~double_buffer:true ~depth:true ~alpha:true ();
   Glut.initWindowSize ~w ~h;
-  Glut.createWindow ~title;
+  ignore @@ Glut.createWindow ~title;
   Glut.idleFunc ~cb:(Some Glut.postRedisplay)
 
 (* Initialize OpenGL rendering options *)
@@ -34,15 +34,16 @@ let initView ~w ~h =
 let initEngine ~state ~w ~h =
   initDisplay ~w ~h ~title:"Cornell CS Course Roster";
   initView ~w ~h;
-  Glut.keyboardFunc ~cb:(appController state arrowKeys);
-  Glut.displayFunc (fun () -> Interface.render ());
+  Glut.specialFunc ~cb:(appController state arrowKeys);
+  Glut.displayFunc (fun () -> Interface.render !state);
   Glut.mainLoop
 
 (* This is the first function executed by OCaml *)
 (* We init the game object reference and pass it to the engine *)
 let () =
   ignore @@ Glut.init Sys.argv;
-  let run = initEngine ~w:750 ~h:600 in
+  let state = ref (Interface.init ()) in
+  let run = initEngine ~state ~w:750 ~h:600 in
   run ()
 
 (* LablGL graphics window initialization code credit to:
