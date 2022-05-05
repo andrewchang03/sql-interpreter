3cd74d52307451b23132e5a31e8a1ea08c342565
cathli66 <cl893@cornell.edu>
2022-05-05 00:43:19 -0400

    merging main with old GUI branch

--- src/table.mli
+++ src/table.mli
@@ -1,9 +1,8 @@
 open Command
 
-(** Representation of static table data.
-
-    This module represents the data stored in csv files. It handles
-    loading of that data as well as querying the data. *)
+(** Representation of static table data. This module represents the data
+    stored in csv files. It handles loading of that data as well as
+    querying the data. *)
 
 type t
 (** The abstract type of values representing tables. *)
@@ -11,56 +10,22 @@ type t
 val select : Csv.t -> string list -> Csv.t
 (** [select table col_names] grab columns in col_names from the table *)
 
-val select_all : (string * Csv.t) list -> string -> Csv.t
-(** [select_all tables table_name] returns the whole queried table *)
+val select_all : ('a * 'b) list -> 'a -> 'b
 
-val create_table : string -> (string * Command.data_type) list -> Csv.t
-(** [create_table table_name cols] creates a new .csv file with name
-    [table_name] populated with [cols], which describes the name and
-    data type of each column. Returns: the newly created table. *)
+val create_table : string -> unit
+(** [create_table f] is the name of the file that the empty table is
+    created in. *)
 
 val drop_table : ('a * 'b) list -> 'a -> ('a * 'b) list
 (** [drop_table tables name] deletes the table with [name] from
     [tables]. *)
-
-val alter_table_add :
-  (string * Csv.t) list ->
-  string ->
-  string ->
-  data_type ->
-  (string * Csv.t) list
-(** [alter_table_add lst t col col_type] adds a column to table [t] in
-    [lst] with name [col] and type [col_type]. *)
-
-val alter_table_drop :
-  (string * Csv.t) list ->
-  string ->
-  string ->
-  data_type ->
-  (string * Csv.t) list
-(** [alter_table_drop lst t col col_type] drops the column of table [t]
-    in [lst] with name [col] and type [col_type]. Requires: [col] must
-    exist in the table. Raises: Illegal exception if [col] is not in the
-    table. *)
-
-val alter_table_modify :
-  (string * Csv.t) list ->
-  string ->
-  string ->
-  data_type ->
-  (string * Csv.t) list
-(** [alter_table_modify lst t col col_type] modifies the column of table
-    [t] in [lst] with name [col] and type [col_type]. Requires: [col]
-    must exist in the table. Raises: Illegal exception if [col] is not
-    in the table. *)
 
 val update :
   Csv.t -> string list -> string list -> ('a -> bool) -> Csv.t
 (** [update t s v c] is the updated table with the columns [s] updated
     to the values [v] where the condition [c] is true. *)
 
-val insert :
-  string -> (string * Command.data_type) list -> string list -> Csv.t
+val insert : string -> string list -> string list -> unit
 (** [insert t c v] is the table [t] with a new row with values [v] in
     the respective columns [c] inserted at the beginning of the table*)
 
